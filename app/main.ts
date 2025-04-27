375d90507b3fb33e140ae09300a31bb792a2fd36
Daniel Carvalho <daniel.carvalho@maxmilhas.com.br>
2025-04-27 17:59:29 -0300

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import * as net from "net";
+import { readFileSync } from 'fs';
 
 const server = net.createServer((socket) => {
   socket.on("close", () => {
@@ -6,18 +7,14 @@ const server = net.createServer((socket) => {
   });
 
   socket.on('data', (request) => {
-    const [requestLine, ...headers] = request.toString().split('\r\n\r\n'); 
+    const [requestLine, ...headers] = request.toString().split('\r\n'); 
+    const [httpVerb, rawPath, httpVersion] = requestLine.split(' ').filter(c => c !== '');
+    const path = rawPath.split('/').filter(c => c !== '');
 
-    const [httpVerb, rawPath, httpVersion] = requestLine.split(' ').filter(c => c !== '')
-    const path = rawPath.split('/').filter(c => c !== '')
-
-    console.log(httpVerb, httpVersion);
-    // console.log(headers);
-    // console.log(path)
     if(path[0] === 'echo'){
       const text = path[1];
-
-      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${text.length}\r\n\r\n${text}`);
+      let responseHeaders = 'Content-Type: text/plain\r\nContent-Length: ${text.length}\r\n';
+      socket.write(`HTTP/1.1 200 OK\r\n${responseHeaders}\r\n${text}`);
     }
     else if(path[0] === 'user-agent'){
       let responseHeaders = 'Content-Type: text/plain\r\n';
@@ -28,17 +25,32 @@ const server = net.createServer((socket) => {
           body = headerValue.trim();
           const headerLength = body.length;
           
-          responseHeaders += `Content-Length:${headerLength}\r\n`
+          responseHeaders += `Content-Length:${headerLength}\r\n`;
         }
       }
 
       socket.write(`HTTP/1.1 200 OK\r\n${responseHeaders}\r\n${body}`);
     }
+    else if(path[0] === 'files'){
+      let fileName = path[1];
+
+      const args = process.argv.slice(2);
+      const absPath = args[args.length - 1];
+      const filePath = `${absPath}${fileName}`;
+
+      try {
+        const fileContent = readFileSync(filePath);
+        let responseHeaders = `Content-Type: application/octet-stream\r\nContent-Length: ${fileContent.length}\r\n`;
+        socket.write(`HTTP/1.1 200 OK\r\n${responseHeaders}\r\n${fileContent.toString()}`);
+      } catch (error) {
+        socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+      }
+    }
     else if(path.length === 0){
-      socket.write('HTTP/1.1 200 OK\r\n\r\n')
+      socket.write('HTTP/1.1 200 OK\r\n\r\n');
     }
     else{
-      socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
+      socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
     }
   })
 });
