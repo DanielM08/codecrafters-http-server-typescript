09d7da5eb5932d29d4413e3aa96067d252bcc70e
Daniel Carvalho <daniel.carvalho@maxmilhas.com.br>
2025-04-27 15:40:02 -0300

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,20 +1,38 @@
 import * as net from "net";
 
-console.log("Logs from your program will appear here!");
-
 const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
   });
 
   socket.on('data', (request) => {
-    const [httpVerb, reqTarget, ...rest] = request.toString().split(' '); 
+    const [requestLine, ...headers] = request.toString().split('\r\n'); 
 
-    const path = reqTarget.split('/').filter(c => c !== '')
+    const [httpVerb, rawPath, httpVersion] = requestLine.split(' ').filter(c => c !== '')
+    const path = rawPath.split('/').filter(c => c !== '')
+
+    console.log(httpVerb, httpVersion);
+    // console.log(headers);
+    // console.log(path)
     if(path[0] === 'echo'){
       const text = path[1];
 
       socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${text.length}\r\n\r\n${text}`);
+    }
+    else if(path[0] === 'user-agent'){
+      let responseHeaders = 'Content-Type: text/plain\r\n';
+      let body = '';
+      for(const item of headers){
+        const [headerField, headerValue] = item.split(':');
+        if(headerField.trim().toLowerCase() === 'user-agent'){
+          body = headerValue.trim();
+          const headerLength = body.length;
+          
+          responseHeaders += `Content-Length:${headerLength}\r\n`
+        }
+      }
+
+      socket.write(`HTTP/1.1 200 OK\r\n${responseHeaders}\r\n${body}`);
     }
     else if(path.length === 0){
       socket.write('HTTP/1.1 200 OK\r\n\r\n')
